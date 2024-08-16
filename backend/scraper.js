6d2fd7f51aedfad92a887394a3b34be97897083b
Alejandro Arco <alejandroarpu@gmail.com>
2024-08-16 16:56:55 +0200

    feat: add new file scraper.js with scraping process, new endpoints /api/health and /api/scraping

---
+++ backend/scraper.js
@@ -0,0 +1,27 @@
+const axios = require('axios') // library to do http requests
+const cheerio = require('cheerio') // library to manipulate HTML
+const baseUrl = 'https://news.ycombinator.com/'
+
+const getScraping = () => {
+    return axios.get(baseUrl)
+        .then(response => 
+            {
+                // works just like $ jQuery function
+                const $ = cheerio.load(response.data)
+                const entries = $('tr.athing').map((_,element) => { // get all the tr.athing in the page
+                    const titlesText = $(element).find('td.title span.titleline a').first().text() // get the first a element that contains the title
+                    const subTitleText =  $(element).next('tr').find('td.subtext span.subline') // get sublines tat contains score and comments
+                    const score = subTitleText.find('span.score').text()
+                    const comments = subTitleText.find('a').last().text()
+                    return { pos: _ + 1, title: titlesText, score: score, comments: comments}
+                })
+                .toArray() // to transform jQuery elements to JS array
+                
+                return entries
+            }
+        )
+}
+
+module.exports = {
+    getScraping
+}
